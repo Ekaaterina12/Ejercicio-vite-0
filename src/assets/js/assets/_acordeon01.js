768c5cad81a3b9fbd126db6709ec7c662b30b661
ekaaterina12 <e.goncharova@arefor.com>
2026-06-17 13:38:01 +0200

    nuevo7

--- src/assets/js/assets/_acordeon01.js
+++ src/assets/js/assets/_acordeon01.js
@@ -1,3 +1,16 @@
 export function acordeon01(){
     console.log("entramos")
 }
+
+document.querySelectorAll(".boton").forEach((btn) => {
+    btn.addEventListener("click", () => {
+
+        const parrafo = btn.parentElement.querySelector(".parrafo");
+
+        parrafo.classList.toggle("open");
+
+        btn.textContent = parrafo.classList.contains("open")
+            ? "leer menos"
+            : "leer más";
+    });
+});
