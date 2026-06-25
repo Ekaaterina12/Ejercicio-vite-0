3b5be9a0c3434c88c69c76b45b15d497904f9179
ekaaterina12 <e.goncharova@arefor.com>
2026-06-25 09:47:48 +0200

    fix: Resolve merge conflicts in observer and dark mode styles

--- src/assets/js/assets/_observer.js
+++ src/assets/js/assets/_observer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //“Voy a montar un sistema que vigila la página”
 export default function observer () {
 
@@ -36,34 +35,3 @@ export default function observer () {
 
     console.log({sections});    
 }
-=======
-export default function observer() {
-  function loadElements(entradas) {
-    // Recorremos las entradas observada por el observer
-    entradas.forEach((entrada) => {
-      //Comprobar si la section ha sido intersectada
-      if (entrada.isIntersecting) {
-        //Seleccionamos todos los elementos con la clase fadeOut
-        const fadeOutElements = entrada.target.querySelectorAll(".fadeOut");
-        // Los recorremos
-        fadeOutElements.forEach((element) => {
-          // Quitamos la clase fadeOut y agregamos la fadeIN
-          element.classList.remove("fadeOut");
-          element.classList.add("fadeIn");
-        });
-      }
-    });
-  }
-  //Creamos la instancia del observador y su configuración
-  const observador = new IntersectionObserver(loadElements, {
-    threshold: 0.1,
-  });
-  // Selecionamos las sections para ser observadas
-  const sections = document.querySelectorAll("section");
-
-  sections.forEach((section) => {
-    // Le decimos al observador que observe cada section
-    observador.observe(section);
-  });
-}
->>>>>>> 4fda60056066f7e0f8d02cf7f4fb1b096129fbd5
